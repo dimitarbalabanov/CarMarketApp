12fc13ac6fefdf9e09c59293509b7f05a884643b
Dimitar Balabanov <dimitar.balabanov@gmail.com>
2020-04-23 22:09:51 +0300

    Worked on the front end - Layout, Index and Create listing views

---
+++ Web/CarMarket.Web/Controllers/ListingsController.cs
@@ -0,0 +1,26 @@
+﻿namespace CarMarket.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ListingsController : Controller
+    {
+        [Authorize]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        //[HttpPost]
+        //[Authorize]
+        //public async Task<IActionResult> Create()
+        //{
+        //    return null;
+        //}
+    }
+}
