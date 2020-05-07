ca5b393ca27d75986bd0ec73c438c0ec83d34551
Dimitar Balabanov <dimitar.balabanov@gmail.com>
2020-05-07 21:27:08 +0300

    Added a column for MakeId in the Listing Entity as it will lead to a better searching.

--- Data/CarMarket.Data.Models/Listing.cs
+++ Data/CarMarket.Data.Models/Listing.cs
@@ -31,6 +31,10 @@
 
         public Condition Condition { get; set; }
 
+        public int MakeId { get; set; }
+
+        public virtual Make Make { get; set; }
+
         public int ModelId { get; set; }
 
         public virtual Model Model { get; set; }
---
+++ Data/CarMarket.Data/Migrations/20200507182345_AddedMakeIdColumnToListing.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarMarket.Data.Migrations
+{
+    public partial class AddedMakeIdColumnToListing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MakeId",
+                table: "Listings",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Listings_MakeId",
+                table: "Listings",
+                column: "MakeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Listings_Makes_MakeId",
+                table: "Listings",
+                column: "MakeId",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Listings_Makes_MakeId",
+                table: "Listings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Listings_MakeId",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "MakeId",
+                table: "Listings");
+        }
+    }
+}
